242555cc7ebe21976499729b7a60825ff009f785
hzemouri <hocem.zemouri@on-x.com>
2020-06-26 21:41:08 +0200

    Projet terminé

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Scanner;
+import java.time.Duration;
+import java.util.*;
 
 public class Main {
     static String course[][] = null;
@@ -9,15 +9,8 @@ public class Main {
     static int nbreCoureurs = 0;
     static String infosHeaders[] = {"Nom", "Numero", "Etat", "Temps"};
     final static SimpleDateFormat FORMATTER = new SimpleDateFormat("HH:mm:ss");
-    final static Calendar CALENDAR = Calendar.getInstance();
 
-
-    public static void main(String[] args) {
-
-        System.out.println("|\t Nom \t|\t N° \t|\t Etat \t\t|\t Temps \t|");
-        System.out.println(
-                "-----------------------------------------------------------------");
-        System.out.println("|\t david \t|\t 33 \t|\t arrivé \t|\t 12h23 \t|");
+    public static void main(String[] args) throws ParseException {
 
         System.out.println("####################################################################");
         System.out.println("###############  Bienvenue au Tour de France 2020  #################");
@@ -36,8 +29,10 @@ public class Main {
             System.out.println("Veuillez saisir les informations du coureur N°" + (i + 1)
                     + " sous format: Nom,Numéro,Etat (arrivé ou abandon ou disqualification)");
             infos = sc.nextLine();
-            infos += "," + FORMATTER.format(CALENDAR.getTime() ) + ",";
-            System.out.println(infos);
+            Date date = new Date(System.currentTimeMillis());
+            String time = FORMATTER.format(date);
+            infos += "," + time;
+
             infosTab = infos.trim().split(",");
             course[i] = infosTab;
 
@@ -46,9 +41,9 @@ public class Main {
         afficherTableau();
         boolean mainLoop = true;
 
-        int choice;
+        int choix;
         do {
-            System.out.println("Tour de france 2020\n");
+
             System.out.print("0.) Quitter \n");
             System.out.print("1.) Afficher les coureurs.\n");
             System.out.print("2.) Afficher le classement provisoire.\n");
@@ -58,48 +53,176 @@ public class Main {
             System.out.print("6.) Enregistrer le temps d'arrivée d'un coureur.\n");
             System.out.print("7.) Obtenir le temps d'un coureur.\n");
             System.out.print("8.) Obtenir l'écart de temps entre deux coureurs donnés\n");
-            System.out.print("\nEnter Your Menu Choice: ");
-            choice = sc.nextInt();
+            System.out.print("\nEntrez votre choix: ");
+
+            choix = sc.nextInt();
             sc.nextLine();
 
-            switch (choice) {
+            switch (choix) {
                 case 0:
                     System.out.println("Fermeture du programme...");
                     System.exit(0);
                     break;
                 case 1:
-                    System.out.println("choice 1");
+                    System.out.println("Afficher les coureurs");
                     afficherTableau();
+                    System.out.println();
                     break;
                 case 2:
-                    // do something
                     System.out.println("Classement provisoire");
+                    classementProvisoire();
+                    System.out.println();
                     break;
 
                 case 3:
-                    // do something
                     System.out.println("Enregistrer une arrivée");
+                    enregistrerArrivee();
                     break;
 
                 case 4:
-                    // do something
                     System.out.println("Enregistrer un abandon");
+                    enregistrerAbandon();
+                    System.out.println();
                     break;
 
                 case 5:
-                    // do something
-                    System.out.println("choice 5");
+                    System.out.println("Enregistrer une disqualification");
+                    enregistrerDisqualification();
+                    System.out.println();
                     break;
-
                 case 6:
+                    System.out.println("Enregistrer le temps d'arrivée d'un coureur");
                     enregistrerTempsCoureur();
+                    System.out.println();
+                    break;
+                case 7:
+                    System.out.println("Obtenir le temps d'un coureur");
+                    obtenirTemps();
+                    System.out.println();
+                    break;
+                case 8:
+                    System.out.println("Trouver l'écart de temps entre deux coureurs");
+                    ecartTemps();
+                    System.out.println();
                     break;
                 default:
-                    System.out.println(choice + " is not a valid Menu Option! Please Select Another.");
+                    System.out.println(choix + " n'est pas valide, choisissez-en un autre.");
 
             }
-        } while (choice != 0 /* Quitter la loupe quand le choix = 0 */);
+        } while (choix != 0 /* Quitter la loupe quand le choix = 0 */);
 
+    }
+
+    public static void classementProvisoire() {
+        Arrays.sort(course, new Comparator<String[]>() { // sort the array
+
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                try {
+                    return FORMATTER.parse(o1[3]).compareTo(FORMATTER.parse(o2[3]));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                return -1;
+            }
+        });
+        for(int i = 0; i < course.length; i++){ // display the result
+            for(int j = 0; j < course[i].length; j++)
+            {
+                System.out.print(course[i][j]);
+                if(j < course[i].length - 1) System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void obtenirTemps() {
+        afficherTableau();
+        System.out.println("Saisir le numéro du coureur pour obtenir son temps :");
+        String numero = sc.nextLine();
+        int indice = find(course, numero);
+        // if indice < course.length : => l'indice existe
+        if (indice != -1 && indice < course.length) {
+            System.out.println("Temps du coureur N°" + numero + " : " + course[indice][3]);
+        } else {
+            System.out.println("Le numéro que vous avez saisi n'existe pas !");
+        }
+    }
+
+    private static void ecartTemps() throws ParseException {
+        System.out.println("Ecart entre deux coureurs");
+        afficherTableau();
+        System.out.println("Saisir le numéro du 1er coureur: ");
+        String coureur1 = sc.nextLine();
+        System.out.println("Saisir le numéro du 1er coureur: ");
+        String coureur2 = sc.nextLine();
+
+        int indice1 = find(course, coureur1);
+        int indice2 = find(course, coureur2);
+
+        if (indice1 != -1 && indice1 < course.length && indice2 != -1 && indice2 < course.length ) {
+            String temps1 = course[indice1][3];
+            String temps2 = course[indice2][3];
+            Date date1 = FORMATTER.parse(temps1);
+            Date date2 = FORMATTER.parse(temps2);
+            long difference = date2.getTime() - date1.getTime();
+            System.out.println("\n l'écart entre les deux coureurs est de " + difference/60000 + " minutes.");
+        } else {
+            System.out.println("L'un des coureurs n'existe pas !");
+        }
+
+    }
+
+
+    public static void enregistrerDisqualification() {
+        afficherTableau();
+        System.out.println("Saisir le numéro du coureur pour enregistrer disqualifié :");
+        String numero = sc.nextLine();
+        int indice = find(course, numero);
+        // if indice < course.length : => l'indice existe
+        if (indice != -1 && indice < course.length) {
+            course[indice][2] = "disqualifié";
+
+            course[indice][3] = "";
+
+            afficherTableau();
+        } else {
+            System.out.println("Le numéro que vous avez saisi n'existe pas !");
+        }
+    }
+
+    public static void enregistrerAbandon() {
+        afficherTableau();
+        System.out.println("Saisir le numéro du coureur pour enregistrer son abandon :");
+        String numero = sc.nextLine();
+        int indice = find(course, numero);
+        // if indice < course.length : => l'indice existe
+        if (indice != -1 && indice < course.length) {
+            course[indice][2] = "abandon";
+
+            course[indice][3] = "";
+
+            afficherTableau();
+        } else {
+            System.out.println("Le numéro que vous avez saisi n'existe pas !");
+        }
+    }
+
+    public static void enregistrerArrivee() {
+        afficherTableau();
+        System.out.println("Saisir le numéro du coureur pour enregistrer son arrivéé :");
+        String numero = sc.nextLine();
+        int indice = find(course, numero);
+        // if indice < course.length : => l'indice existe
+        if (indice != -1 && indice < course.length) {
+            course[indice][2] = "arrive";
+
+            course[indice][3] = FORMATTER.format(new Date(System.currentTimeMillis()));
+
+            afficherTableau();
+        } else {
+            System.out.println("Le numéro que vous avez saisi n'existe pas !");
+        }
     }
 
     public static void afficherTableau() {
@@ -140,7 +263,6 @@ public class Main {
         for (int i = 0; i < a.length; i++)
             if (a[i][1].equals(target))
                 return i;
-
         return -1;
     }
 
